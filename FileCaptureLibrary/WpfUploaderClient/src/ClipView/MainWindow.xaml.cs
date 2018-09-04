4365e5d2e45c5ea1b9e8347e465a38ada596924e
Bailey Miller <bmiller@susquehannasoftware.com>
2018-09-04 08:08:14 -0400

    Video Preview

--- FileCaptureLibrary/WpfUploaderClient/src/ClipView/MainWindow.xaml.cs
+++ FileCaptureLibrary/WpfUploaderClient/src/ClipView/MainWindow.xaml.cs
@@ -35,13 +35,62 @@ namespace WpfUploaderClient
             _Watcher.OnFileRemoved += FileListUpdated;
 
             _Watcher.StartWatching();
+
+            VideoPreview.MediaEnded += MediaEnded;
         }
 
-        private void FileListUpdated(object sender, FileInfo File)
+        private void MediaEnded(object sender, RoutedEventArgs e)
+        {
+            VideoPreview.Stop();
+            // Reset to preview mode
+            VideoPreview.Position = TimeSpan.FromMilliseconds(0);
+        }
+
+        private async void FileListUpdated(object sender, FileInfo File)
         {
             var list = _Watcher.GetAllFiles();
             // Refresh List of Files
             Debug.WriteLine($"{list.Count()} files currently in watch.");
+
+
+            await Dispatcher.InvokeAsync(()=> 
+            {
+                VideoList.Children.Clear();
+
+                if (VideoPreview.Source != null && VideoPreview.Source.OriginalString == File.FullName)
+                {
+                    VideoPreview.Close();
+                    VideoPreview.Visibility = Visibility.Collapsed;
+                }
+
+                foreach (var video in list)
+                {
+                    var btn = new Button()
+                    {
+                        Content = $"{video.Name}",
+                        Padding = new Thickness(0, 25, 0, 0),
+                        Tag = video
+                    };
+
+                    btn.Click += VideoPreviewLaunch;
+
+                    // Loop and add buttons for the scroll view
+                    VideoList.Children.Add(btn);
+                }
+            });
+
+            
+        }
+
+        private async void VideoPreviewLaunch(object sender, RoutedEventArgs e)
+        {
+            var getVideoInfo = ((sender as Button).Tag as FileInfo);
+            await Dispatcher.InvokeAsync(()=> 
+            {
+                VideoPreview.Visibility = Visibility.Visible;
+                VideoPreview.Source = new Uri(getVideoInfo.FullName);
+                VideoPreview.Play();
+            });
         }
 
         private void Manual_Upload_Clicked(object sender, RoutedEventArgs e)
